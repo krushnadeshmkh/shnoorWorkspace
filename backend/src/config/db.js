a6958c2506058cd50ca2488cf0bc93b60670d700
krushnadeshmkh <krishnadeshmukh176@gmail.com>
2026-07-31 16:29:03 +0530

    changes

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -3,20 +3,38 @@ require('dotenv').config();
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false },  
-  max: 200,
-  idleTimeoutMillis: 30000,
-  connectionTimeoutMillis: 10000,
+  ssl: { rejectUnauthorized: false },
+  max: 500,
+  idleTimeoutMillis: 10000,
+  connectionTimeoutMillis: 20000,
   maxUses: 7500,
+  allowExitOnIdle: false,
+  statement_timeout: 10000,
+  query_timeout: 10000,
+  application_name: 'myapp',
 });
 
 pool.on('connect', () => {
   console.log('Connected to PostgreSQL');
 });
 
+pool.on('acquire', () => {
+  console.log('Client acquired from pool');
+});
+
+pool.on('remove', () => {
+  console.log('Client removed from pool');
+});
+
 pool.on('error', (err) => {
   console.error('Unexpected error on idle client', err);
-  process.exit(-1);
+});
+
+process.on('SIGINT', async () => {
+  console.log('Closing database pool...');
+  await pool.end();
+  console.log('Database pool closed');
+  process.exit(0);
 });
 
 module.exports = pool;
